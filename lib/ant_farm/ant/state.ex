d75b74bf6938774d99c83cffe3c8f2a8ece2d741
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-01 16:02:17 -0300

    feat: move ant by direction angle and speed

--- lib/ant_farm/ant/state.ex
+++ lib/ant_farm/ant/state.ex
@@ -1,11 +1,29 @@
 defmodule AntFarm.Ant.State do
-  @type position :: {integer, integer}
+  @type position :: {float, float}
 
   @type t :: %__MODULE__{
           id: integer,
-          position: position
+          position: position,
+          speed: float,
+          direction: 0..360
         }
 
   defstruct id: nil,
-            position: {0, 0}
+            position: {0.0, 0.0},
+            speed: 0.0,
+            direction: 0
+
+  def move(%__MODULE__{speed: speed, direction: angle, position: {x, y}} = ant) do
+    x = Float.round(x + speed * dX(angle), 2)
+    y = Float.round(y + speed * dY(angle), 2)
+
+    ant
+    |> Map.replace!(:position, {x, y})
+  end
+
+  defp dX(angle),
+    do: :math.sin(angle * :math.pi() / 180)
+
+  defp dY(angle),
+    do: :math.cos(angle * :math.pi() / 180)
 end
---
+++ test/ant_farm/ant/state_test.exs
@@ -0,0 +1,22 @@
+defmodule AntFarm.Ant.StateTest do
+  use ExUnit.Case
+  doctest AntFarm.Ant.State
+
+  alias AntFarm.Ant.State, as: Ant
+
+  describe "ant state" do
+    test "move/1 do not changes ant position if speed is zero" do
+      before_move = %Ant{speed: 0}
+      after_move = before_move |> Ant.move()
+
+      assert before_move.position == after_move.position
+    end
+
+    test "move/1 changes ant position if speed is more than zero" do
+      before_move = %Ant{speed: 1}
+      after_move = before_move |> Ant.move()
+
+      assert before_move.position != after_move.position
+    end
+  end
+end
