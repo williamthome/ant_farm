5a3baec60c70e72628038d17d9b3a2548120c2aa
William Fank Thomé <willegp88@gmail.com>
2021-10-01 13:58:03 -0300

    feat: add a child spec for ant server

--- lib/ant_farm/ant.ex
+++ lib/ant_farm/ant.ex
@@ -3,10 +3,12 @@ defmodule AntFarm.Ant do
 
   alias __MODULE__.State, as: Ant
 
+  @me __MODULE__
+
   # Client
 
-  def start_link(id \\ __MODULE__),
-    do: GenServer.start_link(__MODULE__, %Ant{id: id}, name: id)
+  def start_link(id \\ @me),
+    do: GenServer.start_link(@me, %Ant{id: id}, name: id)
 
   def get_state(ant_pid),
     do: GenServer.call(ant_pid, :state)
@@ -15,6 +17,12 @@ defmodule AntFarm.Ant do
 
   def init(ant),
     do: {:ok, ant}
+
+  def child_spec(id),
+    do: %{
+      id: id,
+      start: {@me, :start_link, id}
+    }
 
   def handle_call(:state, _from, ant),
     do: {:reply, ant, ant}
