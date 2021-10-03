9c95773f5bf479e676b973034176a7d0ba833ba6
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-03 13:51:31 -0300

    feat: get colony supervisor childs as ant state

--- lib/ant_farm/ant/colony.ex
+++ lib/ant_farm/ant/colony.ex
@@ -21,6 +21,27 @@ defmodule AntFarm.Ant.Colony do
   def populate(count \\ 1),
     do: for(_ <- 1..count, do: start_child(%Ant{id: Incrementer.increment()}))
 
+  def ants,
+    do:
+      @me
+      |> DynamicSupervisor.which_children()
+      |> Task.async_stream(&get_ant_state/1)
+      |> filter_successful_tasks()
+
+  defp get_ant_state({_id, ant_pid, _type, _modules}),
+    do: ant_pid |> AntServer.get_state()
+
+  defp filter_successful_tasks(tasks),
+    do:
+      tasks
+      |> Enum.filter(fn task ->
+        case task do
+          {:ok, _result} -> true
+          {:exit, _reason} -> false
+        end
+      end)
+      |> Enum.map(fn {:ok, result} -> result end)
+
   # Server
 
   def init(:ok),
