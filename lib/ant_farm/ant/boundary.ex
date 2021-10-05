03df0af11d2a6d211a0d585800d1d2fded1f0d70
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-04 22:56:12 -0300

    feat: schedule ant action

--- lib/ant_farm/ant/boundary.ex
+++ lib/ant_farm/ant/boundary.ex
@@ -4,6 +4,8 @@ defmodule AntFarm.Ant.Boundary do
   alias AntFarm.Ant.Core, as: Ant
 
   @me __MODULE__
+  @walking_timeout_range 30..100
+  @resting_timeout_range 1_000..2_000
 
   # Client
 
@@ -27,14 +29,46 @@ defmodule AntFarm.Ant.Boundary do
 
   # Server
 
-  def init(ant),
-    do: {:ok, ant}
+  def init(%Ant{action: action} = ant) do
+    action |> schedule()
+
+    {:ok, ant}
+  end
 
   def child_spec(%Ant{id: id} = ant),
     do: %{
       id: name(id),
       start: {@me, :start_link, [ant]}
     }
+
+  defp schedule(:walking = action), do: action |> schedule(@walking_timeout_range)
+  defp schedule(:resting = action), do: action |> schedule(@resting_timeout_range)
+
+  defp schedule(action, timeout_range),
+    do:
+      self()
+      |> Process.send_after({:perform_action, action}, timeout_range |> Enum.random())
+
+  def handle_info({:perform_action, action}, ant) do
+    ant =
+      ant
+      |> maybe_move?(action)
+      |> Map.replace!(:action, action)
+
+    random_action() |> schedule()
+
+    {:noreply, ant}
+  end
+
+  defp random_action do
+    case Enum.random(0..100) < 98 do
+      true -> :walking
+      false -> :resting
+    end
+  end
+
+  defp maybe_move?(%Ant{} = ant, :walking), do: ant |> Ant.move()
+  defp maybe_move?(%Ant{} = ant, _action), do: ant
 
   def handle_call(:state, _from, ant),
     do: {:reply, ant, ant}
